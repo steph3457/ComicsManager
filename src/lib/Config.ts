5a5b4299cf3015a41f44224e1185d195785e556c
steph3457 <steph3457@gmail.com>
2017-03-14 22:00:35 +0100

    add allowed publishers in the config view

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -2,14 +2,22 @@ import { Publisher } from "./Publisher";
 export class Config {
     comicsPath: string;
     comicVineAPI: string;
-    publishers: {[name: string] : Publisher} = {};
-    constructor(config:Config) {
-        this.comicsPath = config.comicsPath;
-        this.comicVineAPI = config.comicVineAPI;
-        for(var publisher in config.publishers)
-        {
-            this.publishers[publisher] = new Publisher(config.publishers[publisher]);
+    publishers: { [name: string]: Publisher } = {};
+    constructor(config: Config) {
+        if (config) {
+            this.comicsPath = config.comicsPath;
+            this.comicVineAPI = config.comicVineAPI;
+            for (var publisher in config.publishers) {
+                this.publishers[publisher] = new Publisher(config.publishers[publisher]);
+            }
         }
+    }
+    public getPublishers(): Publisher[] {
+        var publishers: Publisher[] = [];
+        for (var p in this.publishers) {
+            publishers.push(this.publishers[p]);
+        }
+        return publishers.sort();
     }
 
 }
