3c1efeaefdb7a58e11e173d993019923a1f8c310
Stéphane Gosselin <stephane.gosselin@soyhuce.fr>
2017-09-07 17:15:41 +0200

    test notifications

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,17 +1,27 @@
-import { Form  } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { LibraryService } from '../library.service';
+import { Form } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { LibraryService } from "../library.service";
+import { NotificationsService } from "angular2-notifications";
 
 @Component({
-  selector: 'app-config',
-  templateUrl: './config.component.html',
-  styleUrls: ['./config.component.css'],
+    selector: "app-config",
+    templateUrl: "./config.component.html",
+    styleUrls: ["./config.component.css"]
 })
 export class ConfigComponent implements OnInit {
+    public options = {
+        position: ["bottom", "left"],
+        timeOut: 5000,
+        lastOnBottom: true
+    };
 
-  constructor(private libraryService: LibraryService) { }
+    constructor(
+        private libraryService: LibraryService,
+        private notificationsService: NotificationsService
+    ) {}
 
-  ngOnInit() {
-  }
-
+    test() {
+        this.notificationsService.alert("test", "test2");
+    }
+    ngOnInit() {}
 }
